eae83e2ad946e98e52af19f5b51c4e975a7b7ecb
Robert Snyder <r.snyder49@gmail.com>
2019-01-18 01:28:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-import { combineReducers } from "redux";
- 
-const rootReducer = combineReducers({
-  authors: authorsReducer,
-  books: booksReducer
-});
- 
-export default rootReducer;
- 
 function booksReducer(state = [], action) {
   let idx;
   switch (action.type) {
@@ -14,21 +5,6 @@ function booksReducer(state = [], action) {
       return [...state, action.book];
  
     case "REMOVE_BOOK":
-      idx = state.indexOf(action.id);
-      return [...state.slice(0, idx), ...state.slice(idx + 1)];
- 
-    default:
-      return state;
-  }
-}
- 
-function authorsReducer(state = [], action) {
-  let idx;
-  switch (action.type) {
-    case "ADD_AUTHOR":
-      return [...state, action.author];
- 
-    case "REMOVE_AUTHOR":
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
  
